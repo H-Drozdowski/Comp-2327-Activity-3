f44e1cc210b0ee6f3b7d928607edc4afbfa52000
H-Drozdowski <hdrozdowski@rrc.ca>
2026-03-01 13:55:45 -0600

    feat: Completed the Payment class, added the init method & pay_bill method.

--- payment/payment.py
+++ payment/payment.py
@@ -1,4 +1,50 @@
 """This module defines the Payment class."""
 
-__author__ = ""
-__version__ = ""
+from patterns.strategy.payment_strategy import PaymentStrategy
+from billing_account.billing_account import BillingAccount
+from payee.payee import Payee
+
+__author__ = "Hudson Drozdowski"
+__version__ = "3.14.2"
+
+class Payment():
+    """Represents a payment."""
+    
+    def __init__(self, strategy : PaymentStrategy):
+        """Initializes a new instance of the Payment class.
+        
+        Args: 
+            strategy (PaymentStrategy): A object representing the 
+                payment strategy.
+        
+        Raises:
+            - ValueError: A ValueError is raised when the argument value
+            of strategy is not of the PaymentStrategy type.
+        """
+
+        if not isinstance(strategy, PaymentStrategy):
+            raise ValueError("Invalid Strategy")
+        else:
+            self.__strategy = strategy
+
+    def pay_bill(self, account : BillingAccount, payee : Payee, 
+                 amount : float) -> str:
+        """Pays the bill processes the payment and returns the payment
+        confirmation message.
+        
+        Args:
+            account (BillingAccount): An instance of the BillingAccount
+                class representing a users billing account.
+            payee (Payee): An instance of the Payee class representing 
+                the receiver of the payment.
+            amount (float): The amount of money being paid to the payee. 
+        
+        Returns:
+            str: Returns a string representing the payment and new 
+            balance.
+        """
+
+        payment_confirmation = self.__strategy.process_payment(account, payee, amount)
+
+        return payment_confirmation
+
